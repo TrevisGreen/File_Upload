4911ac63ca9fc6496f62c28e7d9c9ddd25d2fd04
TrevisGreen <Trevis.Green@gmail.com>
2023-02-26 21:31:13 -0600

    Created ComponentConfig class

--- src/main/java/com/erlandgreen/fileupload/config/ComponentConfig.java
+++ src/main/java/com/erlandgreen/fileupload/config/ComponentConfig.java
@@ -1,4 +1,16 @@
 package com.erlandgreen.fileupload.config;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "com.erlandgreen.fileupload.model",
+        "com.erlandgreen.fileupload.dao",
+        "com.erlandgreen.fileupload.utils"})
+@PropertySource("classpath:fileUpload.properties")
 public class ComponentConfig {
+
 }
