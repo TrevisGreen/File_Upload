489e253cd5ec23974184bfbe0f6013cc1655ab7c
TrevisGreen <Trevis.Green@gmail.com>
2023-02-26 21:07:26 -0600

    Created ImageDao class

--- src/main/java/com/erlandgreen/fileupload/dao/ImageDao.java
+++ src/main/java/com/erlandgreen/fileupload/dao/ImageDao.java
@@ -1,4 +1,16 @@
 package com.erlandgreen.fileupload.dao;
 
+import com.erlandgreen.fileupload.model.Image;
+
+import java.util.List;
+
 public interface ImageDao {
+
+    public List<Image> list();
+
+    public Image create(Image image);
+
+    public Image get(Long id);
+
+    public void delete(Image image);
 }
