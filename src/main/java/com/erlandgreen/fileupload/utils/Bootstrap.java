48aae7bb065b65e41f890f4874f56aac461c3027
TrevisGreen <Trevis.Green@gmail.com>
2023-02-26 20:48:45 -0600

    Created Bootstrap class

--- src/main/java/com/erlandgreen/fileupload/utils/Bootstrap.java
+++ src/main/java/com/erlandgreen/fileupload/utils/Bootstrap.java
@@ -1,4 +1,20 @@
 package com.erlandgreen.fileupload.utils;
 
-public class Bootstrap {
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger log = LoggerFactory.getLogger(Bootstrap.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent e) {
+        log.info("Verifying application");
+        log.info("fileUpload Application is up");
+    }
 }
