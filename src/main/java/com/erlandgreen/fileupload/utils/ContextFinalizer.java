de219d5ee05e73d2c47f10f3a0e5adb9cc82296d
TrevisGreen <Trevis.Green@gmail.com>
2023-02-26 21:03:21 -0600

    Created ContextFinalizer class

---
+++ src/main/java/com/erlandgreen/fileupload/utils/ContextFinalizer.java
@@ -0,0 +1,42 @@
+package com.erlandgreen.fileupload.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Set;
+
+public class ContextFinalizer implements ApplicationListener<ContextClosedEvent> {
+
+    private static final Logger log = LoggerFactory.getLogger(ContextFinalizer.class);
+
+    @Override
+    public void onApplicationEvent(ContextClosedEvent event) {
+        log.info("Stopping connections");
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        Driver d = null;
+        while (drivers.hasMoreElements()) {
+            try {
+                d = drivers.nextElement();
+                DriverManager.deregisterDriver(d);
+            } catch (SQLException ex) {
+                log.warn(String.format("Error deregistering driver %s", d), ex);
+            }
+        }
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
+        for(Thread t : threadArray) {
+            if(t.getName().contains("Abandoned connection cleanup thread")) {
+                synchronized (t) {
+                    t.stop();
+                }
+            }
+        }
+        log.info("Finished stopping connections");
+    }
+}
